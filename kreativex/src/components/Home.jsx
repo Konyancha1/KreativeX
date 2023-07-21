c65461af986bc743b43e9596ce4785f13a6d3afe
Konyancha1 <kmutua791@gmail.com>
2023-07-22 02:08:42 +0300

    Added services

--- kreativex/src/components/Home.jsx
+++ kreativex/src/components/Home.jsx
@@ -39,22 +39,22 @@ const Home = () => {
                         succeed in the digital space
                     </p>
                 </div>
-                <div className='w-[80%] mx-auto'>
+                <div className='w-[w-90%] md:w-[80%] mx-auto'>
                     <div className='grid lg:flex grid-cols-1 md:grid-cols-2 lg:flex-row mx-auto gap-6 mt-10'>
                         <div class='bg-white p-5 h-72 md:w-3/4 w-2/3 mx-auto mt-5 rounded-lg flex flex-col justify-center items-center'>
-                            <h1 class='text-center font-bold text-xl text-red-500 mb-6'>Content Creation</h1>
+                            <h1 class='text-center font-bold text-lg md:text-xl text-red-500 mb-6'>Content Creation</h1>
                             <p class='text-center'>Develop and Produce Valuable and Relevant Content for You To Attract and Retain Customers</p>
                         </div>
                         <div class='bg-white p-5 h-72 md:w-3/4 w-2/3 mx-auto mt-5 rounded-lg flex flex-col justify-center items-center'>
-                            <h1 class='text-center font-bold text-xl text-red-500 mb-6'>Website Development, Data Analytics and SEO</h1>
+                            <h1 class='text-center font-bold text-lg md:text-xl text-red-500 mb-6'>Website Development, Data Analytics and SEO</h1>
                             <p class='text-center'>Design, Build, and Maintain Websites for A Wide Range of Clients, from Small Businesses to Large Corporations</p>
                         </div>
                         <div class='bg-white p-5 h-72 md:w-3/4 w-2/3 mx-auto mt-5 rounded-lg flex flex-col justify-center items-center'>
-                            <h1 class='text-center font-bold text-xl text-red-500 mb-6'>Financial Analytics</h1>
+                            <h1 class='text-center font-bold text-lg md:text-xl text-red-500 mb-6'>Financial Analytics</h1>
                             <p class='text-center'>Provide Financial Analysis, Advice, and Consulting Services to Businesses, Organizations, and Individuals.</p>
                         </div>
                         <div class='bg-white p-5 h-72 md:w-3/4 w-2/3 mx-auto mt-5 rounded-lg flex flex-col justify-center items-center'>
-                            <h1 class='text-center font-bold text-xl text-red-500 mb-6'>Talent Training and Management</h1>
+                            <h1 class='text-center font-bold text-lg md:text-xl text-red-500 mb-6'>Talent Training and Management</h1>
                             <p class='text-center'>Designing and Delivering Training Programs to Help Upcoming Creatives Acquire the Knowledge and Skills They Need to Excel</p>
                         </div>
                     </div>
@@ -63,7 +63,7 @@ const Home = () => {
             <h1 className='text-2xl md:text-4xl w-full md:w-[70%] text-white leading-[1.2] font-quicksand font-semibold text-center mx-auto mb-10'>
                 Companies That Have Trusted Us
             </h1>
-            <div class='grid grid-cols-3 gap-4 mx-auto w-[50%] mb-20'>
+            <div class='grid grid-cols-3 gap-4 mx-auto w-[80%] md:w-[50%] mb-20'>
                 <img src={ALU} alt='ALU.png' className='md:w-3/5 w-[80%]'/>
                 <img src={VC} alt='VC.png' className='md:w-3/5 w-[80%]' />
                 <img src={GoTell} alt='GOTELL.png' className='md:w-3/5 w-[80%]'/>
@@ -71,8 +71,8 @@ const Home = () => {
                 <img src={AKR} alt='AKR.png' className='md:w-3/5 w-[80%]'/>
                 <img src={ST} alt='StudentSafaris.png' className='md:w-3/5 w-[80%]'/>
             </div>
-            <footer className='px-16 h-10 mx-auto flex items-center w-full bg-white'>
-                <p className='mx-auto text-base'>© 2023 KreativeX. All rights reserved.</p>
+            <footer className='bottom-0 px-16 h-10 mx-auto flex items-center w-full bg-white'>
+                <p className='mx-auto text-sm md:text-base'>© 2023 KreativeX. All rights reserved.</p>
             </footer>
         </div>
     )
